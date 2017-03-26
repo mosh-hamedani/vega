1e1e73d07d5ee9f8cd5f6b90421e83b59b27b108
Moshfegh Hamedani <moshfegh@live.com.au>
2017-03-27 09:18:52 +1100

    Add vehicle.

---
+++ Models/Vehicle.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace vega.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
