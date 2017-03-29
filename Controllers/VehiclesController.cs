595491a3a52f39a123ab5a01afbcaebb5a914857
Moshfegh Hamedani <moshfegh@live.com.au>
2017-03-29 13:50:01 +1100

    Change the return type of Create and Update vehicle actions.

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -33,7 +33,14 @@ namespace vega.Controllers
         context.Vehicles.Add(vehicle);
         await context.SaveChangesAsync();
 
-        var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
+        vehicle = await context.Vehicles
+          .Include(v => v.Features)
+            .ThenInclude(vf => vf.Feature)
+          .Include(v => v.Model)
+            .ThenInclude(m => m.Make)
+          .SingleOrDefaultAsync(v => v.Id == vehicle.Id);        
+
+        var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
         return Ok(result);
     }
@@ -44,7 +51,12 @@ namespace vega.Controllers
         if (!ModelState.IsValid)
           return BadRequest(ModelState);
 
-        var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+        var vehicle = await context.Vehicles
+          .Include(v => v.Features)
+            .ThenInclude(vf => vf.Feature)
+          .Include(v => v.Model)
+            .ThenInclude(m => m.Make)
+          .SingleOrDefaultAsync(v => v.Id == id);
 
         if (vehicle == null)
           return NotFound();
@@ -54,7 +66,7 @@ namespace vega.Controllers
 
         await context.SaveChangesAsync();
 
-        var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
+        var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
         return Ok(result);
     }
