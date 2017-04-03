d9547d111e07f028b57aaaf525bde27ccfc94150
Moshfegh Hamedani <moshfegh@live.com.au>
2017-04-04 09:19:10 +1000

    Bind dynamic check boxes.

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -10,7 +10,9 @@ export class VehicleFormComponent implements OnInit {
   makes: any[]; 
   models: any[];
   features: any[];
-  vehicle: any = {};
+  vehicle: any = {
+    features: []
+  };
 
   constructor(private vehicleService: VehicleService) { }
 
@@ -26,5 +28,14 @@ export class VehicleFormComponent implements OnInit {
     var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
     this.models = selectedMake ? selectedMake.models : [];
     delete this.vehicle.modelId;
+  }
+
+  onFeatureToggle(featureId, $event) {
+    if ($event.target.checked)
+      this.vehicle.features.push(featureId);
+    else {
+      var index = this.vehicle.features.indexOf(featureId);
+      this.vehicle.features.splice(index, 1);
+    }
   }
 }
