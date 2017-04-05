e256226f6381fc30660510c694c6a81a7a82f793
Moshfegh Hamedani <moshfegh@live.com.au>
2017-04-05 13:20:23 +1000

    Populate the form.

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -53,8 +53,10 @@ export class VehicleFormComponent implements OnInit {
       this.makes = data[0];
       this.features = data[1];
 
-      if (this.vehicle.id)
+      if (this.vehicle.id) {
         this.setVehicle(data[2]);
+        this.populateModels();
+      }
     }, err => {
       if (err.status == 404)
         this.router.navigate(['/home']);
@@ -71,9 +73,14 @@ export class VehicleFormComponent implements OnInit {
   } 
 
   onMakeChange() {
+    this.populateModels();
+
+    delete this.vehicle.modelId;
+  }
+
+  private populateModels() {
     var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
     this.models = selectedMake ? selectedMake.models : [];
-    delete this.vehicle.modelId;
   }
 
   onFeatureToggle(featureId, $event) {
