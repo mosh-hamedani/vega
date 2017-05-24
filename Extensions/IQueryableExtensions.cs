1143a557791118935294bbe059676421cb9ef6e7
Mosh Hamedani <moshfegh@live.com.au>
2017-05-24 10:04:08 -0700

    Optimize queries.

--- Extensions/IQueryableExtensions.cs
+++ Extensions/IQueryableExtensions.cs
@@ -8,6 +8,18 @@ namespace vega.Extensions
 {
     public static class IQueryableExtensions
     {
+
+      public static IQueryable<Vehicle> ApplyFiltering(this IQueryable<Vehicle> query, VehicleQuery queryObj)
+      {
+        if (queryObj.MakeId.HasValue)
+          query = query.Where(v => v.Model.MakeId == queryObj.MakeId.Value);
+
+        if (queryObj.ModelId.HasValue)
+          query = query.Where(v => v.ModelId == queryObj.ModelId.Value);
+
+        return query; 
+      }
+
       public static IQueryable<T> ApplyOrdering<T>(this IQueryable<T> query, IQueryObject queryObj, Dictionary<string, Expression<Func<T, object>>> columnsMap)
       {
         if (String.IsNullOrWhiteSpace(queryObj.SortBy) || !columnsMap.ContainsKey(queryObj.SortBy))
--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -49,15 +49,9 @@ namespace vega.Persistence
       var query = context.Vehicles
         .Include(v => v.Model)
           .ThenInclude(m => m.Make)
-        .Include(v => v.Features)
-          .ThenInclude(vf => vf.Feature)
         .AsQueryable();
 
-      if (queryObj.MakeId.HasValue)
-        query = query.Where(v => v.Model.MakeId == queryObj.MakeId.Value);
-
-      if (queryObj.ModelId.HasValue)
-        query = query.Where(v => v.ModelId == queryObj.ModelId.Value);
+      query = query.ApplyFiltering(queryObj);
 
       var columnsMap = new Dictionary<string, Expression<Func<Vehicle, object>>>()
       {
@@ -65,7 +59,6 @@ namespace vega.Persistence
         ["model"] = v => v.Model.Name,
         ["contactName"] = v => v.ContactName
       };
-
       query = query.ApplyOrdering(queryObj, columnsMap);
       
       result.TotalItems = await query.CountAsync();
